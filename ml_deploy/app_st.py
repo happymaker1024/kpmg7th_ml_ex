77305b252451388837e77c2de9d89b6c7ee80140
happymaker <happymaker1024@gmail.com>
2025-12-02 13:09:14 +0900

    app_st.py 모델 path 변경

--- ml_deploy/app_st.py
+++ ml_deploy/app_st.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent
 # 모델 로드 함수
 @st.cache_resource  # 자원 캐싱 기능 
 def load_model():
-    model_path = f"{BASE_DIR}/models/iris_model_rfc.pkl"
+    model_path = BASE_DIR / "models" / "iris_model_rfc.pkl"
     # with open('models/iris_model_rfc.pkl', 'rb') as f:
     with open(model_path, 'rb') as f:
         model = pickle.load(f)
